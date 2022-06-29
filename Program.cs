ea3b86d0eade1d1242d25a14cafca839c8f62c75
Artem <temka6397@mail.ru>
2022-06-29 22:40:47 +0500

    Vtoroe zadanie

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿Console.Write("Введите первое число:");
+﻿//Console.Write("Введите первое число:");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число:");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2)
+//{
+//    Console.WriteLine("Первое - большее,второе - меньшее");
+//}
+//else 
+//{
+//   Console.WriteLine("Второе - большее,первое - меньшее"); 
+//}
+
+Console.Write("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2)
-{
-    Console.WriteLine("Первое - большее,второе - меньшее");
+Console.Write("Введите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ( number1 > number2)
+{ 
+    max = number1;
 }
 else 
-{
-   Console.WriteLine("Второе - большее,первое - меньшее"); 
+{ 
+    max = number2;
 }
+if ( number3 > max)
+{ 
+    max = number3;
+}
+Console.WriteLine(max);
