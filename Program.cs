48b7b7a38276ffd09da2888f9437a3c4023768ce
Artem <temka6397@mail.ru>
2022-06-29 22:42:50 +0500

    Vers 2.0

--- Program.cs
+++ Program.cs
@@ -1,15 +1,4 @@
-﻿//Console.Write("Введите первое число:");
-//int number1 = Convert.ToInt32(Console.ReadLine());
-//Console.Write("Введите второе число:");
-//int number2 = Convert.ToInt32(Console.ReadLine());
-//if (number1 > number2)
-//{
-//    Console.WriteLine("Первое - большее,второе - меньшее");
-//}
-//else 
-//{
-//   Console.WriteLine("Второе - большее,первое - меньшее"); 
-//}
+﻿
 
 Console.Write("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
